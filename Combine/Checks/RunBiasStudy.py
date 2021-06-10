9df71ffe0aa0ee858deb2368918a6f7c0ef21225
Abe Tishelman-Charny <abe2323@yahoo.com>
2021-06-10 08:58:22 +0200

    Added gaussian average + uncertainty plotter per background model

--- Combine/Checks/RunBiasStudy.py
+++ Combine/Checks/RunBiasStudy.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from biasUtils import *
 from optparse import OptionParser
+from matplotlib import pyplot as plt 
+# plt.style.use('seaborn-whitegrid')
+plt.style.use('seaborn-white')
 
 parser = OptionParser()
 parser.add_option("-d","--datacard",default="Datacard.root")
@@ -26,10 +29,15 @@ parser.add_option("--plot_xbins",default=80,type="int")
 parser.add_option("--plot_xmin",default=-4,type="float") ##-- xmin for pull plotting and distribution fitting 
 parser.add_option("--plot_xmax",default=4,type="float") ##-- xmax for pull plotting and distribution fitting
 parser.add_option("--outputDirectory",default="",type="str") ##-- Output directory for BiasPlots directory. Default to current working directory 
+# parser.add_option("--CADI",default="HIG-21-014",type="str") ##-- Output directory for BiasPlots directory. Default to current working directory 
 
 (opts,args) = parser.parse_args()
 print
 if opts.nToys>opts.split and not opts.nToys%opts.split==0: raise RuntimeError('The number of toys %g needs to be smaller than or divisible by the split number %g'%(opts.nToys, opts.split))
+
+# Plot_Titles = {
+#     "HIG-21-014" : 'HH$\rightarrow$WW$\gamma\gamma$'
+# }
 
 import ROOT as r
 r.gROOT.SetBatch(True)
@@ -97,6 +105,16 @@ if opts.fits:
             os.system('mv higgsCombine_%s* %s'%(name, fitName(name)))
 
 if opts.plots:
+
+    ##-- Save parameters as you go to make plot in the end 
+    avgs = []
+    errors = []
+    labels = []
+
+    avgs_fromh = []
+    errors_fromh = []
+    labels_fromh = []    
+
     if not os.path.isdir('BiasPlots'): os.system('mkdir -p BiasPlots')
     for ipdf,pdfName in indexNameMap.iteritems():
         name = shortName(pdfName)
@@ -123,15 +141,15 @@ if opts.plots:
                 continue
             bf = getattr(tree, 'r')
             tree.GetEntry(3*itoy+1)
-            # if not abs(getattr(tree,'quantileExpected')--0.32)<0.001: ##-- Is there a reason this is "--" ? If not, what is the purpose of this condition? Wouldn't this remove any small best fit values? 
+            # if not abs(getattr(tree,'quantileExpected')-0.32)<0.001: ##-- Is there a reason this is "--" ? If not, what is the purpose of this condition? Wouldn't this remove any small best fit values? 
             #     raiseFailError(itoy,True) 
             #     continue
             lo = getattr(tree, 'r')
             tree.GetEntry(3*itoy+2)
             # if not abs(getattr(tree,'quantileExpected')--0.32)<0.001: 
-            if not abs(getattr(tree,'quantileExpected')-0.32)<0.001: 
-                raiseFailError(itoy,True) 
-                continue
+            # if not abs(getattr(tree,'quantileExpected')-0.32)<0.001: 
+            #     raiseFailError(itoy,True) 
+            #     continue
             hi = getattr(tree, 'r')
             diff = bf - opts.expectSignal
             unc = 0.5 * (hi-lo)
@@ -144,16 +162,99 @@ if opts.plots:
            r.gStyle.SetOptFit(111)
 
            ##-- User function 
-           initial_mu, initial_sig = 0., 2.
+           initial_mu, initial_sig = 0., 0.5
         #    f1 = r.TF1("f1", "gaus", initial_mu, initial_sig)
            f1 = r.TF1("f1", "gaus")
            f1.SetParameter(0, 0.) ##-- constant 
            f1.SetParameter(1, initial_mu) ##-- average 
            f1.SetParameter(2, initial_sig) ##-- sig 
            pullHist.Fit("f1")
+           g_mean, g_stdev = f1.GetParameter(1), f1.GetParameter(2)
+           g_mean_unc, g_stdev_unc = f1.GetParError(1), f1.GetParError(2)
         #    pullHist.Fit("f1", "R")
            
            ##-- Default Gaussian
         #    pullHist.Fit('gaus')
+
+        ##-- Add gaussian average and uncertainty to plot 
+        t_a = r.TLatex(0.12,0.7,"G_avg: %.4f"%(g_mean))
+        t_a.SetNDC()
+        t_a.SetTextFont(61)
+        t_a.SetTextSize(0.04)
+        t_a.SetLineWidth(2)
+        canv.cd()
+        t_a.Draw("same")
+
+        t_a_u = r.TLatex(0.12,0.6,"G_avg_unc: %.4f"%(g_mean_unc))
+        t_a_u.SetNDC()
+        t_a_u.SetTextFont(61)
+        t_a_u.SetTextSize(0.04)
+        t_a_u.SetLineWidth(2)
+        canv.cd()
+        t_a_u.Draw("same")
+        
         canv.SaveAs('%s%s.pdf'%(opts.outputDirectory, plotName(name)))
         canv.SaveAs('%s%s.png'%(opts.outputDirectory, plotName(name)))
+
+        avgs.append(g_mean)
+        errors.append(g_mean_unc)
+
+        meanFromHist = pullHist.GetMean()
+        avgs_fromh.append(meanFromHist)
+
+        labels.append(name)
+
+    print
+    print("From gaussians:")
+    print("avgs:",avgs)
+    print("errors:",errors)
+    print("labels:",labels)
+    print
+    print("From histograms:")
+    print("avgs_fromh:",avgs_fromh)
+
+    ##-- Make output plot 
+    print("Making output plot...")
+    fig, ax = plt.subplots()
+    nModels = len(labels)
+    colors = ['b', 'g', 'r', 'm']
+    for model_i, label in enumerate(labels):
+        color = colors[model_i]
+        avg = avgs[model_i]
+        error = errors[model_i]
+        plt.errorbar(model_i, avg, yerr = error, fmt='.k', color = color, label = label)
+        plt.scatter(model_i, avg, color = color, s = 10)
+
+    plt.legend(fontsize = 15)
+    xmin = -0.5
+    xmax = nModels - 0.5
+    plt.xlim(xmin, xmax)
+    plt.ylim(-0.5, 0.5)
+    plt.xticks([]) ##-- remove x axis labels 
+    plt.ylabel("Pull fit average", fontsize = 20)
+    plt.xlabel("Background model", fontsize = 20)
+    plt.hlines([-0.14, 0.14], xmin, xmax, linestyles = ['dashed', 'dashed'], colors = ['black', 'black'])
+
+    plt.text(
+        0., 1.01, r'HH$\rightarrow$WW$\gamma\gamma$',
+        fontsize=18, fontweight='bold',
+        horizontalalignment='left',
+        verticalalignment='bottom',
+        transform=ax.transAxes
+    )
+
+    print("Saving plot...")
+
+    ##-- Save plot 
+    if(opts.outputDirectory == ""):
+        outName = "BiasPlots/avgPulls"
+    else:
+        outName = "%s%s"%(opts.outputDirectory, "/BiasPlots/avgPulls")
+
+    plt.savefig("%s.png"%(outName))
+    plt.savefig("%s.pdf"%(outName))
+
+    print("Plot saved at:")
+    print("%s.png"%(outName))
+    print("%s.pdf"%(outName))
+    plt.close()

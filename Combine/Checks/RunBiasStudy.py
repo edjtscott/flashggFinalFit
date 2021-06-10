7b92e1e4ab997a2bf73353065e0fb3bb70e67e32
Abe Tishelman-Charny <abe2323@yahoo.com>
2021-06-10 05:38:04 +0200

    Added more bias study flags, removed example commands and notes

--- Combine/Checks/RunBiasStudy.py
+++ Combine/Checks/RunBiasStudy.py
@@ -19,7 +19,14 @@ parser.add_option("--dryRun",action="store_true", default=False)
 parser.add_option("--poi",default="r")
 parser.add_option("--split",default=500,type="int")
 parser.add_option("--selectFunction",default=None)
-parser.add_option("--gaussianFit",action="store_true", default=False)
+
+##-- Plotting
+parser.add_option("--gaussianFit",action="store_true", default=False) 
+parser.add_option("--plot_xbins",default=80,type="int")
+parser.add_option("--plot_xmin",default=-4,type="float") ##-- xmin for pull plotting and distribution fitting 
+parser.add_option("--plot_xmax",default=4,type="float") ##-- xmax for pull plotting and distribution fitting
+parser.add_option("--outputDirectory",default="",type="str") ##-- Output directory for BiasPlots directory. Default to current working directory 
+
 (opts,args) = parser.parse_args()
 print
 if opts.nToys>opts.split and not opts.nToys%opts.split==0: raise RuntimeError('The number of toys %g needs to be smaller than or divisible by the split number %g'%(opts.nToys, opts.split))
@@ -106,8 +113,7 @@ if opts.plots:
             continue 
         tree = tfile.Get('limit')
         print("tree:",tree)
-        # pullHist = r.TH1F('pullsForTruth_%s'%name, 'Pull distribution using the envelope to fit %s'%name, 80, -4., 4.)
-        pullHist = r.TH1F('pullsForTruth_%s'%name, 'Pull distribution using the envelope to fit %s'%name, 80, -50., 50.)
+        pullHist = r.TH1F('pullsForTruth_%s'%name, 'Pull distribution using the envelope to fit %s'%(name), opts.plot_xbins, opts.plot_xmin, opts.plot_xmax)
         pullHist.GetXaxis().SetTitle('Pull')
         pullHist.GetYaxis().SetTitle('Entries')
         for itoy in range(opts.nToys):
@@ -117,11 +123,12 @@ if opts.plots:
                 continue
             bf = getattr(tree, 'r')
             tree.GetEntry(3*itoy+1)
-            if not abs(getattr(tree,'quantileExpected')--0.32)<0.001: 
-                raiseFailError(itoy,True) 
-                continue
+            # if not abs(getattr(tree,'quantileExpected')--0.32)<0.001: ##-- Is there a reason this is "--" ? If not, what is the purpose of this condition? Wouldn't this remove any small best fit values? 
+            #     raiseFailError(itoy,True) 
+            #     continue
             lo = getattr(tree, 'r')
             tree.GetEntry(3*itoy+2)
+            # if not abs(getattr(tree,'quantileExpected')--0.32)<0.001: 
             if not abs(getattr(tree,'quantileExpected')-0.32)<0.001: 
                 raiseFailError(itoy,True) 
                 continue
@@ -129,20 +136,24 @@ if opts.plots:
             diff = bf - opts.expectSignal
             unc = 0.5 * (hi-lo)
             if unc > 0.: 
-                # print("bf:",bf)
-                # print("unc:",unc)
-                # print("float(diff/unc):",float(diff/unc))
                 pullHist.Fill(float(diff/unc))
         canv = r.TCanvas()
         pullHist.Draw()
         if opts.gaussianFit:
            print("Doing gaussian fit")
            r.gStyle.SetOptFit(111)
-           initial_mu, initial_sig = 2, 2
-           f1 = r.TF1("f1", "gaus", initial_mu, initial_sig)
-        #    TF1 *f1 = new TF1("f1", "gaus", 1, 3);
-           pullHist.Fit("f1", "R")
+
+           ##-- User function 
+           initial_mu, initial_sig = 0., 2.
+        #    f1 = r.TF1("f1", "gaus", initial_mu, initial_sig)
+           f1 = r.TF1("f1", "gaus")
+           f1.SetParameter(0, 0.) ##-- constant 
+           f1.SetParameter(1, initial_mu) ##-- average 
+           f1.SetParameter(2, initial_sig) ##-- sig 
+           pullHist.Fit("f1")
+        #    pullHist.Fit("f1", "R")
+           
+           ##-- Default Gaussian
         #    pullHist.Fit('gaus')
-        outDir = "/eos/user/a/atishelm/www/HHWWgg/Bias_Studies/"
-        canv.SaveAs('%s/%s.pdf'%(outDir, plotName(name)))
-        canv.SaveAs('%s/%s.png'%(outDir, plotName(name)))
+        canv.SaveAs('%s%s.pdf'%(opts.outputDirectory, plotName(name)))
+        canv.SaveAs('%s%s.png'%(opts.outputDirectory, plotName(name)))
